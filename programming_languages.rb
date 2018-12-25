25064e53e99aeb9074b6a08ecdd6ffa779ef65b8
Raymond Chow <raychow1944@yahoo.com>
2018-12-25 08:41:36 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -5,13 +5,7 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |language_type, language|
     language.each do |key, value|
-      # new_style = []
-      # new_value = value
-      # new_value[:style] = new_style << language_type 
-      # new_hash[key] = {
-        
-      # }
-      new_hash[key] = 
+      value.each
     end
   end
   new_hash
