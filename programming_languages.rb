e02f668179d589018dbe7c1b9cdda6bb8b8bcd29
Raymond Chow <raychow1944@yahoo.com>
2018-12-25 08:20:21 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -5,6 +5,8 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |language_type, language|
     language.each do |key, value|
+      binding.pry
+      new_hash[]
     end
   end
   new_hash
